a8140857c8f039a53fbc408757fd5f794db3d4d3
jankikansagra03 <janki.kansagra12@gmail.com>
2024-04-02 13:09:47 +0530

    resend

--- Sample_E/Forgot_password_otp.php
+++ Sample_E/Forgot_password_otp.php
@@ -2,12 +2,36 @@
 include_once("guest_header.php");
 
 ?>
-<script>
-    function enableButton() {
-        document.getElementById('r_btn').disabled = false;
-    }
+<script type="text/javascript">
+    // Function to start the countdown timer
+    function startTimer(duration, display) {
+        var timer = duration,
+            minutes, seconds;
+        setInterval(function() {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
 
-    setTimeout(enableButton, 10000);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = 0;
+            }
+        }, 1000);
+    }
+    window.onload = function() {
+        var oneMinute = 60,
+            display = document.getElementById('#timer');
+
+        startTimer(oneMinute, display);
+    };
+
+    function enableButton() {
+        document.getElementById("r_btn").disabled = false;
+    }
+    setTimeout(enableButton, 60000);
 </script>
 <div class="container-fluid">
     <div class="row">
